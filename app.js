d3798383147eb65f2d10d5afccb23cfba68941e2
Rohit Paul <rohitpaul97@gmail.com>
2022-05-02 22:17:22 +0530

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://rohit-library:library.Rohit23@librarycluster.kphbo.mongodb.net/local_library?retryWrites=true&w=majority';
+var url = 'mongodb+srv://rohit-library:library.Rohit23@librarycluster.kphbo.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || url; 
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
